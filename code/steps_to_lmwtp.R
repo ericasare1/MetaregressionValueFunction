518f0c015551777e1e3627901e83e8c0f2ee2078
ericasare1 <eric.asare.kojo@gmail.com>
2021-04-08 22:40:09 -0600

    adding extracted US willingness to pay estimates

--- code/steps_to_lmwtp.R
+++ code/steps_to_lmwtp.R
@@ -14,26 +14,38 @@ exch_rate_us_to_can <- read_csv("data/raw/exc_us_to_can.csv")
 wtp_orginal <- read_csv("data/raw/wtp_raw.csv")
 meta_data <- read_csv("data/Data_for_analysis_15_10.csv")
 
-#Willingness to pay values extracted from original studies (C$ at study year)
-tkac <- 146.98
-trenholm_30mWoodlotRiparian <- 31.56
-trenholm_60mWoodlotRiparian <- 19.38
-trenholm_30mAllLandTypes <- 24.7
-trenholm_60mAllLandTypes <- 14.7
-pattisson <- 
-pattisson <- 
-pattisson <- 
-pattisson <- 
-pattisson <- 
-pattisson <- 
-lantz1 <- 
-lantz2 <-
-rudd1 <- 11.6
-rudd2 <- 23.44
-he_ce <- 482
-he_cv <- 465
-vossler <- 836
+#Willingness to pay values extracted from original Canadian studies (C$ at study year)
+tkac_wtp <- 146.98
+trenholm_wtp_30W <- 31.56     #30m riparian buffer: woodlot
+trenholm_wtp_60W<- 19.38      #60m riparian buffer: woodlot
+trenholm_wtp_30mAll <- 24.7  #30m riparian buffer: all land types (woodlot, agric, residential)
+trenholm_wtp_60mAll <- 14.7 #60m riparian buffer: all land types (woodlot, agric, residential)
+pattisson_wtp_2008l <- 295.1        # Retention at 2008 level
+pattisson_wtp_80 <- 301.65          # retention at 80% of 1968 level
+pattisson_wtp_83 <- 308.31          # retention at 83% of 1968 level
+pattisson_wtp_89 <- 321.46          # retention at 89% of 1968 level
+pattisson_wtp_100 <- 347.78         # retention at 100% of 1968 level
+lantz_wtp1 <- mean(35+222.2+1060.7) # wetland retention
+lantz_wtp2 <- mean(36+227.5+1038)   # wetland retention plus restoration of additional 1000acres
+rudd_wtp1 <- 11.6       # wetlands - some improvements
+rudd_wtp2 <- 23.44      # wetlands - large improvements
+he_wtp_ce <- 482               #estimate from choice experiment
+he_wtp_cv <- 465               #estimate from contingent valuation
+vossler_wtp <- 836
 
+#willingness to pay estimates from additional US studies
+#Johnson, Holland and Yao (2016). Individualised Geocoding in SP Questionnaires: Impl. for survey design and welfare est.
+johnson1_wtp_85 <- 1.09 * (4000/47)    # wtp is 1.09 per 47 vegetated acres: 4000 acres 85% of original 4700 acres
+johnson1_wtp_87 <- 1.09 * (4100/47)    # wtp is 1.09 per 47 vegetated acres: 4100 acres 87% of original 4700 acres
+johnson1_wtp_90 <- 1.09 * (4200/47)    # wtp is 1.09 per 47 vegetated acres: 4200 acres 90% of original 4700 acres
+johnson1_wtp_95 <- 1.09 * (4500/47)    # wtp is 1.09 per 47 vegetated acres: 4500 acres 95% of original 4700 acres
+
+#Johnson, Feurt and Holland (2015). Ecosystem serv and riparian land management in the Merriland, Branch Brook anf Little River Watershed
+johnson2_wtp_85 <- 0.044 * (4000/47)    # wtp is 0.044 per 47 vegetated acres: 4000 acres 85% of original 4700 acres
+johnson2_wtp_87 <- 0.044 * (4100/47)    # wtp is 0.044 per 47 vegetated acres: 4100 acres 87% of original 4700 acres
+johnson2_wtp_95 <- 0.044 * (4500/47)    # wtp is 0.044 per 47 vegetated acres: 4500 acres 95% of original 4700 acres
+
+  
 exchrate <- exch_rate_us_to_can %>% 
 	group_by(Year) %>%
 	summarize(av_monthly_excrate = mean(exchrate)) %>%
