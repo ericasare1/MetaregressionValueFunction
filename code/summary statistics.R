889350be46c1b93007a2c12da90d057ca3e8022f
ericasare1 <eric.asare.kojo@gmail.com>
2021-05-06 04:27:17 -0600

    added ggplot of lnwtp vrsln acreage change

--- code/summary statistics.R
+++ code/summary statistics.R
@@ -21,3 +21,25 @@ grouped_sum <- df_sum %>% group_by(us) %>% descr(stats = "common") %>% tb()
 #saving data
 write_csv(sum_whole, "output/tables/sum_whole_23_3_21.csv")
 write_csv(grouped_sum, "output/tables/grouped__23_3_21.csv")
+
+
+#plot lnwtp vrs lnacres 
+df <- df %>% 
+  mutate(
+  Country = ifelse(us ==1, "US", "Canada")
+)
+df %>% ggplot(aes(x=lnq_change,y=lnwtp, color= Country)) + 
+  geom_point() +
+  stat_smooth(method = "lm", 
+              color = "black",
+              se = FALSE,
+              size = 0.2) +
+  labs(
+    x = "Log (Willingness to Pay)",
+    y = "Log (Wetland Acreage Change)"
+  ) +
+  coord_flip() +
+  theme(text =element_text(size = 14)) +
+  theme_bw()
+
+ggsave("output/lnwtp_vrs_lnchange.png")
